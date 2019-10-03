5f9f33e563990341353b7b7c0027cd90ae589747
rawanah995 <rawanah995@gmail.com>
2019-10-03 08:56:17 +0300

    adding the EJS cars and cats

--- exp/server.js
+++ exp/server.js
@@ -3,8 +3,22 @@ const app = express();
 app.use(express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
 app.get('/', (request, response) => {
     response.send("hi");
+    
+});
+
+app.get('/cars', (request, res) => {
+    res.render('cars');
+});
+
+app.get('/cats', (request, res) => {
+    res.render('cats');
+});
+
+app.get('/cars/new', (request, res) => {
+    res.render('new');
 });
 
 
