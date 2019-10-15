4e4148662b7ebdf9dd0678682d6a29308959271f
rawanah995 <rawanah995@gmail.com>
2019-10-15 17:51:07 +0300

    adding controllers assighnment

--- quoting_modularization/server/config/routes.js
+++ quoting_modularization/server/config/routes.js
@@ -1,45 +1,18 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/quotes', { useNewUrlParser: true });
 
-const QouteSchema = new mongoose.Schema({
-    name: { type: String, maxlength: 20 },
-    qoute: { type: String, minlength: 20 },
-    date: { type: String }
-
-})
-// create an object to that contains methods for mongoose to interface with MongoDB
-const Qoute = mongoose.model('Qoute', QouteSchema);
+const quotes= require("../controllers/qoutes.js")
 
 module.exports = function (app) {
     app.get('/', (req, res) => {
-        Qoute.find()
-            .then(data => {
-                console.log(data)
-                res.render("index", { qoutes: data })
-            })
-            .catch(err => res.json(err));
+        quotes.index(req, res);
     });
 
     app.post('/qoute', (req, res) => {
-        const qoute = new Qoute();
-        qoute.name = req.body.name;
-        qoute.qoute = req.body.qoute;
-        qoute.date = new Date();
-        qoute.save()
-            .then(newQouteData => console.log('qoute created: ', newQouteData))
-            .catch(err => console.log(err));
-
-        res.redirect('/');
+        quotes.create(req, res);
     })
 
 
     app.get('/qoutes', (req, res) => {
-        Qoute.find()
-            .then(data => {
-                console.log(data)
-                res.render("qoutes", { qoutes: data })
-            })
-            .catch(err => res.json(err));
+        quotes.show(req, res);
     })
 
 
